eda398c38d9a99b750f857632c0c8acf791799ce
priyanshu73 <pyakurelpriyanshu73@gmail.com>
2025-08-07 22:20:21 -0400

    what

--- backend/dbConfig.js
+++ backend/dbConfig.js
@@ -1,9 +1,12 @@
+import dotenv from 'dotenv'
+dotenv.config();
+
 const dbConfig = {
-  host: 'localhost',
-  user: 'root', 
-  password: 'B@zing@73', 
-  database: 'disc',
-  port: 3306,
+  host: process.env.DB_HOST ||  'localhost',
+  user: process.env.DB_USER ||  'root',
+  password: process.env.DB_PASSWORD ||  '',
+  database: process.env.DB_NAME ||  'localhost',
+  port: process.env.DB_PORT ||  '3306',
 };
 
 export default dbConfig; 
